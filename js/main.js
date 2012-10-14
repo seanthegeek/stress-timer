81cd02429250a7d4d2bbc4b555348c205ac9d982
Sean Whalen <whalenster@gmail.com>
2012-10-13 23:07:54 -0400

    code cleanup

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ Distributed under the terms of the MIT license.
  http://www.opensource.org/licenses/mit-license.html
 
 This notice shall be included in all copies or substantial portions of the Software.
- */
+*/
 
-// These values below may be edited
+// The values below may be edited
+
+    // The amount of time to allot
     minutes = 3;
     
-    // Volume between 0 and 1
+    // The initial volume level, between 0 and 1
     volume = 0.05;
+    
+    // The volume increases by this amount every second
     volumeStep = 0.01;
     
  // Do not edit below this line! 
@@ -19,7 +23,8 @@ This notice shall be included in all copies or substantial portions of the Softw
 $(document).ready(function(){
     "use strict";
     
-	var remaining, elapsed;
+    // Declare storage
+	var remaining, elapsed, soundPath, item, queue;
 	
 	function draw () {
 		var data = [{color: "black", data: remaining},
@@ -48,7 +53,7 @@ $(document).ready(function(){
 	}
 	
 	function exitFullScreen () {
-			  if (document.exitFullscreen) {
+		 if (document.exitFullscreen) {
 		    document.exitFullscreen();
 		}
 		else if (document.mozCancelFullScreen) {
@@ -60,15 +65,26 @@ $(document).ready(function(){
 	}
 	
 	function reset () {
+	    // Reset the timer
 		remaining = minutes * 60;
 		elapsed = 0;
 		
+		// Stop the ticking sound (if playing)
 		createjs.SoundJS.stop();
+		
+		// Exit full screen (if needed)
 		exitFullScreen();
 		
+		// Update the chart
 		draw();
+		
+		// Hide the loading message
 		$("#message").hide();
+		
+		// Set the status message text
 		$("#status").text(minutes + " minutes");
+		
+		// Show the UI
 		$("#ui").show();
 	}
 	
@@ -78,38 +94,23 @@ $(document).ready(function(){
 	  message.show();
 	}
 	
-	function init () {
-	     var soundPath, item, queue;
-
-		 // Initialize the base path from this document to the Flash Plugin
-		 createjs.FlashPlugin.BASE_PATH = "js/vendor/";
-		
-		// Add the sounds to the queue
-		 soundPath = "sound/34855__jackstrebor__clock-ticking";
-		item = {src:soundPath+".mp3|"+soundPath+".ogg", id:"tick"};
-		
-		// Instantiate a queue.
-		queue = new createjs.PreloadJS();
-		
-		// Plug in SoundJS to handle browser-specific paths
-		queue.installPlugin(createjs.SoundJS);
-		
-		// Load the file
-		queue.onComplete = reset;
-		queue.onFileError = handleFileError;
-		queue.loadFile(item, true);
-	}
-	
 	function tick () {
+	    // Update the timer
 		remaining -= 1;
 		elapsed += 1;
+		
+		// Update the chart
 		draw();
+		
+		// Gradually increase the volume 
 		if (volume < 1) {
 			volume += volumeStep;
 			createjs.SoundJS.setVolume(volume);	
 		}
+		
+		// Repeat until the time runs out
 	  if (remaining > 0) {
-	      // Update every second
+	      // Update every second (1000 milliseconds)
 	      setTimeout(function(){tick();}, 1000);
 	  } else {
 	      reset();
@@ -117,14 +118,39 @@ $(document).ready(function(){
 	}
 	
 	function start () {
+		// Hide the UI
 		$("#ui").hide();
+		
+		// Go full screen (if supported by the browser)
 		fullScreen();
+		
+		// Start looping the ticking sound at the initial volume 
 		createjs.SoundJS.play("tick", null , null, null, -1, volume);
+		
+		// Start the timer loop
 		tick();
 	}
-	   $("#start").click(function (event) {
+	
+     // Initialize the base path from this document to the Flash Plugin
+     createjs.FlashPlugin.BASE_PATH = "js/vendor/";
+    
+    // Add the sounds to the queue
+     soundPath = "sound/34855__jackstrebor__clock-ticking";
+    item = {src:soundPath+".mp3|"+soundPath+".ogg", id:"tick"};
+    
+    // Instantiate a queue.
+    queue = new createjs.PreloadJS();
+    
+    // Plug in SoundJS to handle browser-specific paths
+    queue.installPlugin(createjs.SoundJS);
+    
+    // Load the sound file
+    queue.onComplete = reset;
+    queue.onFileError = handleFileError;
+    queue.loadFile(item, true);
+    
+    // Bind the start function to the start button
+    $("#start").click(function (event) {
         start();
-    });
-
-	init();
+        });
  });
